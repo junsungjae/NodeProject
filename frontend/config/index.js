09385987f54d191dab9b120db06682a489ae953d
sungjae jun <jsjf2000@naver.com>
2021-11-05 19:36:24 +0900

    node + express + vue

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -3,6 +3,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
+const ip = require("ip");
 
 module.exports = {
   dev: {
@@ -10,7 +11,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      'api': {
+        target: ip.address + ':3000/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api':''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
